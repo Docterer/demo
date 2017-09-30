68e663dde7d57f82c2cb54e28d4420fe6b80e495
J0-J0 <726692245@qq.com>
2017-09-30 11:04:43 +0800

    HttpResult类移动位置

---
+++ src/main/java/com/jojo/util/arg/HttpResult.java
@@ -0,0 +1,22 @@
+package com.jojo.util.arg;
+
+import java.util.Map;
+
+public class HttpResult {
+
+	public void setBody(String result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setCode(int statusCode) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setHeaderMap(Map<String, String> headerMap) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
